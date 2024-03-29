a8c7b84eb4b0f5f4887a13b734ee1fc14bd738b1
Melvillian <melvillevt@gmail.com>
2024-03-29 10:32:27 -0400

    first attempt at updating already processed blocks

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ from lib.anki_utils import add_anki_card_to_deck, anki_call
 def main():
     srs_blocks = find_srs_blocks()
     anki_cards = create_anki_cards_from_srs_blocks(srs_blocks)
-    for card in anki_cards:
-        add_anki_card_to_deck(card)
-        # mark_srs_block_as_processed(card.notion_block_id)
+    assert len(anki_cards) == len(srs_blocks)
+
+    for block in srs_blocks:
+        # add_anki_card_to_deck(card)
+        mark_srs_block_as_processed(block)
 
 
 # This makes it so you can run `python main.py` to run this file
